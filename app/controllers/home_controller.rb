4fd9e79b1e723b26742234c868ce1a90a0a8e1ba
x4d3 <git@xade.eu>
2016-02-09 16:29:05 +0000

    improved display

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,13 +1,26 @@
-class HomeController < ApplicationControllern
+class HomeController < ApplicationController
   def index
+    @products = []
     if current_user
       @organization = current_organization
 
       if @organization
         @synchronizations = Maestrano::Connector::Rails::Synchronization.where(organization_id: @organization.id).order(updated_at: :desc).limit(40)
       end
+
+      if @organization.oauth_uid
+        begin
+          @shop_session = ShopifyAPI::Session.new(@organization.oauth_uid, @organization.oauth_token)
+          ShopifyAPI::Base.activate_session(@shop_session)
+          @products = ShopifyAPI::Product.find(:all, :params => {:limit => 10})
+        ensure
+          ShopifyAPI::Base.clear_session
+        end
+      end
     end
-    products = ShopifyAPI::Product.find(:all, :params => {:limit => 10})
+
+
+
   end
 
 
