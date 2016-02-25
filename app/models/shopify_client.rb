c801bdbb124f0ed719ad751971827577d3d0c6fb
x4d3 <git@xade.eu>
2016-02-25 18:19:53 +0000

    manage shopify pagination

--- app/models/shopify_client.rb
+++ app/models/shopify_client.rb
@@ -11,7 +11,7 @@ class ShopifyClient
   def find(external_entity_name)
     ShopifyAPI::Session.temp(@oauth_uid, @oauth_token) do
       # ugly hack, did not find a better way to convert an entity to a  seriable hash (serializable_hash was not recursive)
-      get_shopify_entity_constant(external_entity_name).find(:all).map { |x| JSON.parse(x.to_json) }
+      find_all(get_shopify_entity_constant(external_entity_name)).map { |x| JSON.parse(x.to_json) }
     end
   end
 
@@ -29,6 +29,19 @@ class ShopifyClient
   private
   def get_shopify_entity_constant(external_entity_name)
     "ShopifyAPI::#{external_entity_name}".constantize
+  end
+
+  # shopify paginate its result
+  def find_all(shopify_instance, params = {})
+    params[:limit] ||= 50
+    params[:page] = 1
+    result = []
+    begin
+      entities = shopify_instance.find(:all, :params => params)
+      params[:page] += 1
+      result.push *(entities.to_a)
+    end while entities.length == params[:limit]
+    result
   end
 
 end
