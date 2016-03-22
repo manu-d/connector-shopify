0fe94c19e2ec9f5309747601f2f0896029a23419
x4d3 <git@xade.eu>
2016-03-22 18:02:46 +0000

    correct indentation and commensts on PR #16

--- app/models/entities/item.rb
+++ app/models/entities/item.rb
@@ -41,11 +41,11 @@ class Entities::Item < Maestrano::Connector::Rails::Entity
     mapped_external_entities_with_idmaps.each do |mapped_external_entities_with_idmap|
       parent_connect_id = mapped_external_entities_with_idmap[:idmap].connec_id
       product = mapped_external_entities_with_idmap[:entity]
+      next unless product[:variants]
       product[:variants].each do |variant|
-        variant[:parent_item_id] = parent_connect_id
         idmap = Maestrano::Connector::Rails::IdMap.find_by(external_id: variant[:external_id], connec_entity: self.class.connec_entity_name.downcase, external_entity: 'variant', organization_id: organization.id)
         variants.push({entity: variant, idmap: idmap || create_variant_id_map(variant, organization)})
-      end if product[:variants]
+      end
     end
     # 2/ push the variants
     self.push_entities_to_connec_to(connec_client, variants, self.class.connec_entity_name, organization)
@@ -73,105 +73,105 @@ class Entities::Item < Maestrano::Connector::Rails::Entity
   end
 
   private
-  def create_variant_id_map(variant, organization)
-    Maestrano::Connector::Rails::IdMap.create(external_id: variant[:external_id], connec_entity: connec_entity_name, external_entity: 'variant', organization_id: organization.id, name: variant[:name])
-  end
+    def create_variant_id_map(variant, organization)
+      Maestrano::Connector::Rails::IdMap.create(external_id: variant[:external_id], connec_entity: connec_entity_name, external_entity: 'variant', organization_id: organization.id, name: variant[:name])
+    end
 
-  # regroup the items that are variants (with parentid nil) to their parents in a variants field
-  def group_items_variants(connec_entities)
-    items_with_variant = []
-    # create default value with a mutable empty array
-    item_variants = Hash.new { |h, k| h[k] = [] }
-    connec_entities.each do |item|
-      parent_id = item['parent_item_id']
-      if parent_id
-        item_variants[parent_id].push item
-      else
-        items_with_variant.push item
+    # regroup the items that are variants (with parentid nil) to their parents in a variants field
+    def group_items_variants(connec_entities)
+      items_with_variant = []
+      # create default value with a mutable empty array
+      item_variants = Hash.new { |h, k| h[k] = [] }
+      connec_entities.each do |item|
+        parent_id = item['parent_item_id']
+        if parent_id
+          item_variants[parent_id].push item
+        else
+          items_with_variant.push item
+        end
       end
-    end
-    items_with_variant.each do |parent_item|
-      parent_item['variants'] = item_variants[parent_item['id']]
-      # get the max of the updated time on all the variant
-      parent_item['updated_at'] = parent_item['variants'].map { |x| x['updated_at'].to_time }.push(parent_item['updated_at'].to_time).max.iso8601
-    end
-    items_with_variant
-  end
-
-  class VariantMapper
-    extend HashMapper
-    map from('id'), to('connec_id')
-    map from('external_id'), to('id')
-    map from('name'), to('title')
-
-    map from('code'), to('sku')
-    map from('sale_price/net_amount'), to('price')
-    map from('quantity_available'), to('inventory_quantity', &:to_i)
-
-    map from('weight'), to('weight')
-    map from('weight_unit'), to('weight_unit')
-
-
-    after_normalize do |input, output|
-      # convert description to options
-      options = input['description'].split('|')
-      options.each_with_index do |val, index|
-        output["option#{index+1}".to_sym] = val
+      items_with_variant.each do |parent_item|
+        parent_item['variants'] = item_variants[parent_item['id']]
+        # get the max of the updated time on all the variant
+        parent_item['updated_at'] = parent_item['variants'].map { |x| x['updated_at'].to_time }.push(parent_item['updated_at'].to_time).max.iso8601
       end
-      output[:inventory_management] = input['is_inventoried'] ? 'shopify' : nil
-      output
+      items_with_variant
     end
 
-    after_denormalize do |input, output|
-      index = 0
-      options = []
-      while option = input["option#{index+1}"]
-        options.push option
-        index +=1
+    class VariantMapper
+      extend HashMapper
+      map from('id'), to('connec_id')
+      map from('external_id'), to('id')
+      map from('name'), to('title')
+
+      map from('code'), to('sku')
+      map from('sale_price/net_amount'), to('price')
+      map from('quantity_available'), to('inventory_quantity', &:to_i)
+
+      map from('weight'), to('weight')
+      map from('weight_unit'), to('weight_unit')
+
+
+      after_normalize do |input, output|
+        # convert description to options
+        options = input['description'].split('|')
+        options.each_with_index do |val, index|
+          output["option#{index+1}".to_sym] = val
+        end
+        output[:inventory_management] = input['is_inventoried'] ? 'shopify' : nil
+        output
       end
-      output[:description] = options.join('|')
-      output[:is_inventoried] = input['inventory_management'] == 'shopify'
-      output
-    end
 
-  end
-
-  class ItemMapper
-    extend HashMapper
-    # normalize from Connec to Shopify
-    # denormalize from Shopify to Connec
-    # map from (connect_field) to (shopify_field)
-
-    map from('description'), to('body_html')
-    map from('name'), to('title')
-    map from('/variants'), to('/variants'), using: VariantMapper
-
-    after_normalize do |input, output|
-      variants = output[:variants]
-      if variants.count == 0
-        # generate the default variant
-        variants[0] = VariantMapper.normalize(input)
-        variants[0].delete :option1
-        variants[0].delete :title
+      after_denormalize do |input, output|
+        index = 0
+        options = []
+        while option = input["option#{index+1}"]
+          options.push option
+          index +=1
+        end
+        output[:description] = options.join('|')
+        output[:is_inventoried] = input['inventory_management'] == 'shopify'
+        output
       end
-      output
+
     end
 
-    after_denormalize do |input, output|
-      if output[:variants] && output[:variants].count == 1
-        variant_part = VariantMapper.denormalize(input['variants'][0])
-        output[:code] = variant_part[:code]
-        output[:sale_price] = variant_part[:sale_price]
-        output[:quantity_available] = variant_part[:quantity_available]
-        output[:weight] = variant_part[:weight]
-        output[:weight_unit] = variant_part[:weight_unit]
-        output[:is_inventoried] = variant_part[:is_inventoried]
-        output[:variants] = []
+    class ItemMapper
+      extend HashMapper
+      # normalize from Connec to Shopify
+      # denormalize from Shopify to Connec
+      # map from (connect_field) to (shopify_field)
+
+      map from('description'), to('body_html')
+      map from('name'), to('title')
+      map from('/variants'), to('/variants'), using: VariantMapper
+
+      after_normalize do |input, output|
+        variants = output[:variants]
+        if variants.count == 0
+          # generate the default variant
+          variants[0] = VariantMapper.normalize(input)
+          variants[0].delete :option1
+          variants[0].delete :title
+        end
+        output
       end
-      output
-    end
 
-  end
+      after_denormalize do |input, output|
+        if output[:variants] && output[:variants].count == 1
+          variant_part = VariantMapper.denormalize(input['variants'][0])
+          output[:code] = variant_part[:code]
+          output[:sale_price] = variant_part[:sale_price]
+          output[:quantity_available] = variant_part[:quantity_available]
+          output[:weight] = variant_part[:weight]
+          output[:weight_unit] = variant_part[:weight_unit]
+          output[:is_inventoried] = variant_part[:is_inventoried]
+          output[:variants] = []
+        end
+        output
+      end
+
+    end
 
 end
 
