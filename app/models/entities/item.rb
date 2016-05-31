1213a5aedac9703bafb73a6d30785df50f262638
x4d3 <git@xade.eu>
2016-05-31 10:50:58 +0100

    Fix issue when variant update was deleting other variants

--- app/models/entities/item.rb
+++ app/models/entities/item.rb
@@ -55,23 +55,28 @@ class Entities::Item < Maestrano::Connector::Rails::Entity
     variant = mapped_connec_entity_with_idmap[:entity]
 
     begin
-      title = variant[:product_title]
-      product = {
-          body_html: variant[:body_html],
-          variants: [variant]
-      }
       if idmap.external_id.blank?
+        product = {
+            body_html: variant[:body_html],
+            variants: [variant],
+            title: variant[:product_title]
+        }
         product[:title] =  title
         created_entity = @external_client.update('Product', product)
         idmap.update_attributes(external_id: created_entity['variants'][0]['id'], last_push_to_external: Time.now, message: nil)
         product_id_map = Maestrano::Connector::Rails::IdMap.find_or_create_by(external_id: created_entity['id'], connec_id: idmap.connec_id, connec_entity: self.class.connec_entity_name, external_entity: 'product', organization_id: @organization.id)
-        product_id_map.update_attributes(last_push_to_external: Time.now, message: nil, name: title)
+        product_id_map.update_attributes(last_push_to_external: Time.now, message: nil, name: variant[:product_title])
         return {idmap: idmap}
       else
         variant[:id] = idmap.external_id
         product_id_map = Maestrano::Connector::Rails::IdMap.find_by(connec_id: idmap.connec_id, connec_entity: self.class.connec_entity_name, external_entity: 'product', organization_id: @organization.id)
-        product[:id] = product_id_map.external_id
+        product = {
+            id: product_id_map.external_id,
+            body_html: variant[:body_html]
+        }
+        variant[:product] = product
         @external_client.update('Product', product)
+        @external_client.update('Variant', variant)
         idmap.update_attributes(last_push_to_external: Time.now, message: nil)
         product_id_map.update_attributes(last_push_to_external: Time.now, message: nil)
         nil
